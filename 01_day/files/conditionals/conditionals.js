c083ecfd646c5b21e66370454cb6b990a03f523e
tmax818 <tylermaxwell661@gmail.com>
2022-03-07 14:09:47 -0800

    add conditional prompt

--- 01_day/files/conditionals/conditionals.js
+++ 01_day/files/conditionals/conditionals.js
@@ -1,1 +1,3 @@
 // conditionals.js
+
+// TODO Write a conditional statement that prints "greater" to the console if a variable is greater than 10, "less" if it is less than 10, and "equal" if the variable is equal to 10. 
--- 01_day/files/conditionals/conditionals.py
+++ 01_day/files/conditionals/conditionals.py
@@ -1,1 +1,3 @@
 # conditionals.py
+
+# TODO Write a conditional statement that prints "greater" to the console if a variable is greater than 10, "less" if it is less than 10, and "equal" if the variable is equal to 10. 
