8c0c16c9abbfdac755a7cd89713d4917b2c9677a
tmax818 <tylermaxwell661@gmail.com>
2022-03-07 08:52:29 -0800

    update

---
+++ 01_day/files/data_types/data_types.py
@@ -0,0 +1,28 @@
+
+# booleans
+
+from re import A
+
+
+is_hungry = True
+print(type(is_hungry))
+has_freckles = False
+print(type(has_freckles))
+
+# numbers
+
+age = 39
+print(type(age))
+balance = 101.45
+print(type(balance))
+
+# strings
+
+name = "Ed Im"
+print(name)
+print(type(name))
+
+# None
+
+dont_know_yet = None
+print(type(dont_know_yet))
