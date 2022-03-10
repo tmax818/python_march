efa6bddc542c60fc8cf85f4bcd4d279e7170fdf2
tmax818 <tylermaxwell661@gmail.com>
2022-03-10 07:43:49 -0800

    add day 4 algos

---
+++ algos/04_day/longestPalindromicSubstring.js
@@ -0,0 +1,36 @@
+/* 
+  Longest Palindrome
+  For this challenge, we will look not only at the entire string provided, but also at the substrings within it. Return the longest palindromic substring. 
+  Strings longer or shorter than complete words are OK.
+  All the substrings of "abc" are:
+  a, ab, abc, b, bc, c
+*/
+
+const { isPalindrome } = require("./isPalindrome");
+const str1 = "what up, daddy-o?";
+const expected1 = "dad";
+
+const str2 = "uh, not much";
+const expected2 = "u";
+
+const str3 = "Yikes! my favorite racecar erupted!";
+const expected3 = "e racecar e";
+
+const str4 = "a1001x20002y5677765z";
+const expected4 = "5677765";
+
+const str5 = "a1001x20002y567765z";
+const expected5 = "567765";
+
+/**
+ * Finds the longest palindromic substring in the given string.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {string} The longest palindromic substring from the given string.
+ */
+function longestPalindromicSubstring(str) {}
+
+module.exports = { longestPalindromicSubstring: longestPal };
+
+/*****************************************************************************/
