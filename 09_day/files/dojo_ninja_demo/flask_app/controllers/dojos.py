85844fa7be274617425a6754f717bca90eb4f361
tmax818 <tylermaxwell661@gmail.com>
2022-03-17 16:34:19 -0700

    up

--- 09_day/files/dojo_ninja_demo/flask_app/controllers/dojos.py
+++ 09_day/files/dojo_ninja_demo/flask_app/controllers/dojos.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from flask import render_template,redirect,request,session
 from flask_app import app
 
@@ -7,3 +8,15 @@ from flask_app.models.dojo import Dojo
 def dojos():
     dojos = Dojo.get_all()
     return render_template('dojos.html', dojos = dojos)
+
+@app.route("/create/dojo", methods=["POST"])
+def create_dojo():
+    print(request.form)
+    Dojo.save(request.form)
+    return redirect('/')
+
+@app.route("/dojos/<int:id>")
+def show_dojo(id):
+    data = { 'id': id}
+    dojo = Dojo.get_dojo_with_ninjas(data)
+    return render_template("show_dojo.html", dojo = dojo)
