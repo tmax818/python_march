4c78768c3a316832de6910926304fcb1c3c51998
tmax818 <tylermaxwell661@gmail.com>
2022-03-18 16:49:11 -0700

    up

--- 10_day/files/login_and_reg_core/logreg_demo/flask_app/models/user.py
+++ 10_day/files/login_and_reg_core/logreg_demo/flask_app/models/user.py
@@ -1,7 +1,7 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import flash, re
 
-DATABASE = 'models_schema'
+DATABASE = 'login_reg'
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
 class User:
@@ -15,46 +15,34 @@ class User:
         self.updated_at = data['updated_at']
 
     @classmethod
-    def get_all(cls):
-        query = "SELECT * FROM users;"
-        results = connectToMySQL(DATABASE).query_db(query)
-        users = []
-        for user in results:
-            users.append( cls(user) )
-        return users
-    
-    @classmethod
     def save(cls, data:dict ) -> int:
         query = "INSERT INTO users (first_name, last_name, email, password) VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(password)s);"
         return connectToMySQL(DATABASE).query_db( query, data )
 
+       ## ! used in user validation
     @classmethod
-    def get_one(cls, data:dict) -> object:
-        query = 'SELECT * FROM users WHERE id = %(id)s;'
-        result = connectToMySQL(DATABASE).query_db(query, data)
+    def get_by_email(cls,data:dict) -> object or bool:
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(DATABASE).query_db(query,data)
+        # Didn't find a matching user
+        if len(result) < 1:
+            return False
         return cls(result[0])
 
-    @classmethod
-    def update(cls, data:dict) -> object:
-        query = 'UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id = %(id)s;'
-        return connectToMySQL(DATABASE).query_db(query, data)
 
-    @classmethod
-    def destroy(cls, data:dict) -> object:
-        query = 'DELETE FROM users WHERE id = %(id)s;'
-        return connectToMySQL(DATABASE).query_db(query, data)
-
-#! static method for validation of a user.
     @staticmethod
     def validate_user(user:dict) -> bool:
         is_valid = True # ! we assume this is true
         if len(user['first_name']) < 3:
             flash("Name must be at least 3 characters.")
             is_valid = False
-        if len(user['last_name']) < 2:
+        if len(user['last_name']) < 3:
             flash("Name must be at least 3 characters.")
             is_valid = False
         if not EMAIL_REGEX.match(user['email']): 
             flash("Invalid email address!")
+            is_valid = False
+        if user['password'] != user['confirm-password']:
+            flash("Passwords do not match")
             is_valid = False
         return is_valid
